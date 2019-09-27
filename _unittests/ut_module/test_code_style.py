23a9a1f90376b44bfd90f5ececb48b0ecf05484e
xavier dupré <xavier.dupre@gmail.com>
2019-09-27 11:49:16 +0200

    pep8

--- _unittests/ut_module/test_code_style.py
+++ _unittests/ut_module/test_code_style.py
@@ -12,7 +12,11 @@ class TestCodeStyle(ExtTestCase):
     def test_style_src(self):
         thi = os.path.abspath(os.path.dirname(__file__))
         src_ = os.path.normpath(os.path.join(thi, "..", "..", "src"))
-        check_pep8(src_, fLOG=fLOG,
+        check_pep8(src_,
+                   pylint_ignore=('C0103', 'C1801', 'R0201', 'R1705', 'W0108', 'W0613',
+                                  'C0111', 'C0412', 'W0622', 'W0621', 'C0411',
+                                  'C0415', 'W0107'),
+                   fLOG=fLOG,
                    skip=["email_sender.py:45: W0603",
                          "Redefining built-in ",
                          "Parameters differ from overridden ",
@@ -28,7 +32,8 @@ class TestCodeStyle(ExtTestCase):
         test = os.path.normpath(os.path.join(thi, "..", ))
         check_pep8(test, fLOG=fLOG, neg_pattern="temp_.*",
                    pylint_ignore=('C0103', 'C1801', 'R0201', 'R1705', 'W0108', 'W0613',
-                                  'C0111', 'C0412', 'W0622', 'W0621', 'C0411'),
+                                  'C0111', 'C0412', 'W0622', 'W0621', 'C0411',
+                                  'C0415'),
                    skip=[])
 
 
