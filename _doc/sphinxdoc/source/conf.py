786a8cfed4846ba0b485898a1a5eb7d455f18362
xavier dupré <xavier.dupre@gmail.com>
2021-01-01 11:18:31 +0100

    Update conf.py

--- _doc/sphinxdoc/source/conf.py
+++ _doc/sphinxdoc/source/conf.py
@@ -5,7 +5,7 @@ from pyquickhelper.helpgen.default_conf import set_sphinx_variables
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.split(__file__)[0])))
 
-set_sphinx_variables(__file__, "pymmails", "Xavier Dupré", 2020,
+set_sphinx_variables(__file__, "pymmails", "Xavier Dupré", 2021,
                      "alabaster", None, locals(), add_extensions=None,
                      extlinks=dict(issue=('https://github.com/sdpython/pymmails/issues/%s', 'issue')))
 
