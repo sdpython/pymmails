b441c857693a3c6dc49bd6fa74abf68dcc1fbbe4
xavier dupré <xavier.dupre@gmail.com>
2022-06-05 00:15:23 +0200

    lint

--- _unittests/ut_module/test_code_style.py
+++ _unittests/ut_module/test_code_style.py
@@ -13,7 +13,7 @@ class TestCodeStyle(ExtTestCase):
         thi = os.path.abspath(os.path.dirname(__file__))
         src_ = os.path.normpath(os.path.join(thi, "..", "..", "src"))
         check_pep8(src_,
-                   pylint_ignore=('C0103', 'C1801', 'R0201', 'R1705', 'W0108', 'W0613',
+                   pylint_ignore=('C0103', 'C1801', 'R1705', 'W0108', 'W0613',
                                   'C0111', 'C0412', 'W0622', 'W0621', 'C0411',
                                   'C0415', 'W0107', 'C0209'),
                    fLOG=fLOG,
@@ -31,7 +31,7 @@ class TestCodeStyle(ExtTestCase):
         thi = os.path.abspath(os.path.dirname(__file__))
         test = os.path.normpath(os.path.join(thi, "..", ))
         check_pep8(test, fLOG=fLOG, neg_pattern="temp_.*",
-                   pylint_ignore=('C0103', 'C1801', 'R0201', 'R1705', 'W0108', 'W0613',
+                   pylint_ignore=('C0103', 'C1801', 'R1705', 'W0108', 'W0613',
                                   'C0111', 'C0412', 'W0622', 'W0621', 'C0411',
                                   'C0415', 'C0209'),
                    skip=[])
