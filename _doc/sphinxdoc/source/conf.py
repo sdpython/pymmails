00da169af9b4a9d1cd5e5f8aef8c595e5a955ad5
xavier dupré <xavier.dupre@gmail.com>
2022-12-30 23:37:04 +0100

    Update conf.py

--- _doc/sphinxdoc/source/conf.py
+++ _doc/sphinxdoc/source/conf.py
@@ -7,7 +7,9 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.split(__file__)[0])))
 
 set_sphinx_variables(__file__, "pymmails", "Xavier Dupré", 2022,
                      "alabaster", None, locals(), add_extensions=None,
-                     extlinks=dict(issue=('https://github.com/sdpython/pymmails/issues/%s', 'issue')))
+                     extlinks=dict(issue=(
+                         'https://github.com/sdpython/pymmails/issues/%s',
+                         'issue %s')))
 
 blog_root = "http://www.xavierdupre.fr/app/pymmails/helpsphinx/"
 html_logo = "phdoc_static/project_ico.png"
