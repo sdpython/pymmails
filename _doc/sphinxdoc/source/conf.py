ab517b87ce438ebec61a6adc518be21e0ea6fa56
xavier dupré <xavier.dupre@gmail.com>
2023-02-02 01:41:59 +0100

    documentation

--- _doc/sphinxdoc/source/conf.py
+++ _doc/sphinxdoc/source/conf.py
@@ -5,11 +5,11 @@ from pyquickhelper.helpgen.default_conf import set_sphinx_variables
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.split(__file__)[0])))
 
-set_sphinx_variables(__file__, "pymmails", "Xavier Dupré", 2022,
+set_sphinx_variables(__file__, "pymmails", "Xavier Dupré", 2023,
                      "alabaster", None, locals(), add_extensions=None,
                      extlinks=dict(issue=(
                          'https://github.com/sdpython/pymmails/issues/%s',
                          'issue %s')))
 
 blog_root = "http://www.xavierdupre.fr/app/pymmails/helpsphinx/"
-html_logo = "phdoc_static/project_ico.png"
+html_logo = "_static/project_ico.png"
